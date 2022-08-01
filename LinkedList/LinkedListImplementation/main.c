57861f56d791f98bac86b29aed0d1d35f9548a3f
Aayush000001 <69786427+Aayush000001@users.noreply.github.com>
2022-08-01 17:01:15 +0530

    Linked List Implementation

---
+++ LinkedList/LinkedListImplementation/main.c
@@ -0,0 +1,68 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * author: Aayush Gulia
+ * email: aayushgulia88@gmail.com
+ * website: https://www.aayushgulia.com
+ * dated: 01-08-2022 16:51
+ *
+ * Program to implement Linked List in C programming language
+ */
+
+struct Node{
+    int data;
+    struct Node * next;
+};
+
+struct Node * first;
+
+void Create(int A[], int n){ // n -> length of array.
+    int i;
+    struct Node *t, *last;              // t -> temporary node, last -> last node.
+
+    first = (struct Node *)malloc(sizeof (struct Node));
+
+    first->data = A[0];
+    first->next = NULL;
+
+    last = first;
+
+    for(i = 1; i < n; i++){
+        t = (struct Node *) malloc(sizeof (struct Node));
+        t->data = A[i];
+        t->next = NULL;
+
+        last->next = t;
+        last = t;
+    }
+}
+
+void Display(struct Node * p){
+    while (p){
+        printf("%d\n", p->data);
+        p = p->next;                // goes to next node.
+    }
+}
+
+int Count (struct Node * p){
+    int c = 0;
+    while(p != NULL){
+        c++;
+        p = p->next;
+    }
+
+    return c;
+}
+
+int main(){
+
+    int A[10] = {9,8,1,8,8,1,2,8,4,2};
+    int len = 10;
+
+    Create(A, len);
+    Display(first);
+    printf("%d\n", Count(first));
+
+    return 0;
+}
